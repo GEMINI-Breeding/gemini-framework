f4ae0cbe889806d4f92235f0d5d92f90f170dc0b
pghate-ucd <pghate@ucdavis.edu>
2024-06-20 16:20:37 -0700

    Delete Next JS app to make way for Next UI app

--- gemini/interface/app/src/app/api/settings.tsx
+++
@@ -1,9 +0,0 @@
-const isLocal = process.env.GEMINI_IS_LOCAL;
-const apiHost = isLocal ? 'localhost' : process.env.REST_API_HOST || 'localhost';
-const apiPort = process.env.REST_API_PORT || 5000;
-
-export const apiSettings = {
-    apiPort,
-    apiHost,
-    apiEndpoint: `http://${apiHost}:${apiPort}/api/`
-};
