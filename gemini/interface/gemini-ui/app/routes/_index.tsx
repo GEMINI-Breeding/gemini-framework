2fafa278c341e9329a754a0a462958f842eb0bf7
pghate-ucd <pghate@ucdavis.edu>
2024-07-16 15:20:11 -0700

    Remove Remix Version of the UI

--- gemini/interface/gemini-ui/app/routes/_index.tsx
+++
@@ -1,9 +0,0 @@
-import CenterMessage from "app/components/centermessage/centermessage";
-
-const welcomeMessage = "Welcome to GEMINI, please click on options in the sidebar to get started.";
-
-export default function Home() {
-    return (
-        <CenterMessage message={welcomeMessage} />
-    );
-}
