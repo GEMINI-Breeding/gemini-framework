8940f18a8aca61a4f90b172ed625a173b089920e
pghate-ucd <pghate@ucdavis.edu>
2024-07-19 14:01:58 -0700

    Remove Zustand and React Query

--- gemini/interface/gemini-ui/src/store.ts
+++ gemini/interface/gemini-ui/src/store.ts
@@ -1,28 +1,31 @@
-import {create} from 'zustand';
-import zustymiddleware from 'zustymiddleware';
+import {configureStore} from '@reduxjs/toolkit';
 
-type GEMINIStore = {
-    currentExperiment: any;
-    currentSensor: any;
-    currentTrait: any;
-    currentPlot: any;
-    setCurrentExperiment: (experiment: any) => void;
-    setCurrentSensor: (sensor: any) => void;
-    setCurrentTrait: (trait: any) => void;
-    setCurrentPlot: (plot: any) => void;
-}
 
-const useStore = create<GEMINIStore>(zustymiddleware((set: any) => ({
-    currentExperiment: {},
-    currentSensor: {},
-    currentTrait: {},
-    currentPlot: {},
-    setCurrentExperiment: (experiment: Object) => set({currentExperiment: experiment}),
-    setCurrentSensor: (sensor: Object) => set({currentSensor: sensor}),
-    setCurrentTrait: (trait: Object) => set({currentTrait: trait}),
-    setCurrentPlot: (plot: Object) => set({currentPlot: plot}),
-})));
+// import {create} from 'zustand';
+// import zustymiddleware from 'zustymiddleware';
 
-(window as any).store = useStore;
-export default useStore;
+// type GEMINIStore = {
+//     currentExperiment: any;
+//     currentSensor: any;
+//     currentTrait: any;
+//     currentPlot: any;
+//     setCurrentExperiment: (experiment: any) => void;
+//     setCurrentSensor: (sensor: any) => void;
+//     setCurrentTrait: (trait: any) => void;
+//     setCurrentPlot: (plot: any) => void;
+// }
+
+// const useStore = create<GEMINIStore>(zustymiddleware((set: any) => ({
+//     currentExperiment: {},
+//     currentSensor: {},
+//     currentTrait: {},
+//     currentPlot: {},
+//     setCurrentExperiment: (experiment: Object) => set({currentExperiment: experiment}),
+//     setCurrentSensor: (sensor: Object) => set({currentSensor: sensor}),
+//     setCurrentTrait: (trait: Object) => set({currentTrait: trait}),
+//     setCurrentPlot: (plot: Object) => set({currentPlot: plot}),
+// })));
+
+// (window as any).store = useStore;
+// export default useStore;
 
