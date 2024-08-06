32ca6f83c36d847872d8432c8ec29ebf1c5174be
pghate-ucd <pghate@ucdavis.edu>
2024-08-06 08:40:42 -0700

    Upladed Files Selector update to Table

--- gemini/interface/gemini-ui/src/components/selectors/uploadedfiles.tsx
+++ gemini/interface/gemini-ui/src/components/selectors/uploadedfiles.tsx
@@ -1,29 +1,47 @@
-// import { useListState } from "@mantine/hooks";
+import { Table, ActionIcon } from "@mantine/core";
 import { useGEMINIUploadStore } from "@/store";
-import { ScrollArea } from "@mantine/core";
-import FileCard from "@/components/datacards/filecard";
+import { IconX } from "@tabler/icons-react";
 
-export default function UploadedFilesSelector() {
+export default function UploadedFileSelector() {
   // Get global store state and actions
   const files = useGEMINIUploadStore((state) => state.files);
   const setFiles = useGEMINIUploadStore((state) => state.setFiles);
 
+  // Delete Handler
+  function deleteFile(file: File) {
+    setFiles(files.filter((f) => f !== file));
+  }
+
   // Create Items from the files
-  const items = files.map((file) => {
-    return (
-      <FileCard
-        key={file.name}
-        file={file}
-        onRemove={() => {
-          setFiles(files.filter((f) => f !== file));
-        }}
-      />
-    );
-  });
+  const rows = files.map((file) => (
+    <Table.Tr key={file.name}>
+      <Table.Td>{file.name}</Table.Td>
+      <Table.Td>{file.size}</Table.Td>
+      <Table.Td>
+        <ActionIcon
+          color="red"
+          onClick={() => {
+            deleteFile(file);
+          }}
+        >
+          <IconX />
+        </ActionIcon>
+      </Table.Td>
+    </Table.Tr>
+  ));
 
   return (
     <div>
-      <ScrollArea>{items}</ScrollArea>
+      <Table stickyHeader stickyHeaderOffset={60}>
+        <Table.Thead>
+          <Table.Tr>
+            <Table.Th>File Name</Table.Th>
+            <Table.Th>Size</Table.Th>
+            <Table.Th>Remove</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
     </div>
   );
 }
