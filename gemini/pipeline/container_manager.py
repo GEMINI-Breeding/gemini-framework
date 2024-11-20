e00bc86275812c49a672e99caf1126e6e16a07a8
Pranav Ghate <pranavghate94@gmail.com>
2024-11-20 05:15:39 -0800

    Add Container Manager class

--- gemini/pipeline/container_manager.py
+++ gemini/pipeline/container_manager.py
@@ -3,8 +3,15 @@ from python_on_whales import DockerClient
 from python_on_whales import Container, Network
 
 from pathlib import Path
+from enum import Enum
 
 from gemini.config.settings import GEMINISettings
+
+class GEMINIPipelineStatus(Enum):
+    BUILDING = "BUILDING"
+    RUNNING = "RUNNING"
+    STOPPED = "STOPPED"
+
 
 class GEMINIContainerManager(BaseModel):
 
@@ -15,14 +22,57 @@ class GEMINIContainerManager(BaseModel):
     pipeline_compose_file: str = Path(__file__).parent / "compose.yaml"
     pipeline_env_file: str = Path(__file__).parent / ".env.example"
     pipeline_settings: GEMINISettings = None
+    pipeline_status: GEMINIPipelineStatus = GEMINIPipelineStatus.STOPPED
 
     docker_client: DockerClient = None
+
+    # Containers
+    db_container: Container = None
+    logger_container: Container = None
+    storage_container: Container = None
 
     def build_images(self) -> bool:
         try:
             # Build the images
             self.docker_client.compose.build()
+            self.pipeline_status = GEMINIPipelineStatus.BUILDING
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        
 
+    def start_containers(self) -> bool:
+        try:
+            # Start the containers
+            self.docker_client.compose.up(detach=True)
+            self.pipeline_status = GEMINIPipelineStatus.RUNNING
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        
+    def stop_containers(self) -> bool:
+        try:
+            # Stop the containers
+            self.docker_client.compose.down()
+            self.pipeline_status = GEMINIPipelineStatus.STOPPED
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        
+    def scan_containers(self) -> bool:
+        try:
+            # Get the containers
+            containers = self.docker_client.container.list()
+            for container in containers:
+                if container.name == self.pipeline_settings.GEMINI_DB_CONTAINER_NAME:
+                    self.db_container = container
+                elif container.name == self.pipeline_settings.GEMINI_LOGGER_CONTAINER_NAME:
+                    self.logger_container = container
+                elif container.name == self.pipeline_settings.GEMINI_STORAGE_CONTAINER_NAME:
+                    self.storage_container = container
 
             return True
         except Exception as e:
@@ -30,6 +80,21 @@ class GEMINIContainerManager(BaseModel):
             return False
 
 
+    def get_pipeline_info(self) -> dict:
+        return {
+            "pipeline_status": self.pipeline_status,
+            "containers": {
+                "db": self.db_container,
+                "logger": self.logger_container,
+                "storage": self.storage_container
+            },
+            "images": {
+                "db": self.pipeline_settings.GEMINI_DB_IMAGE_NAME,
+                "logger": self.pipeline_settings.GEMINI_LOGGER_IMAGE_NAME,
+                "storage": self.pipeline_settings.GEMINI_STORAGE_IMAGE_NAME
+            },
+            "networks": self.docker_client.network.list()
+        }
 
     def setup(self) -> bool:
         try:
@@ -47,6 +112,12 @@ class GEMINIContainerManager(BaseModel):
             # Build the images
             self.build_images()
 
+            # Start the containers
+            self.start_containers()
+
+            # Get the containers
+            self.scan_containers()
+
         except Exception as e:
             return False
 
@@ -55,3 +126,4 @@ class GEMINIContainerManager(BaseModel):
 if __name__ == "__main__":
     manager = GEMINIContainerManager()
     manager.setup()
+    print(manager.get_pipeline_info())
